6e3060d5f086ad877c017113d5f0a21ee8557957
Notarin <424c414e4b@gmail.com>
2023-09-16 22:27:58 -0400

    Did some cleanup as well as globalizing sys

--- src/daemon.rs
+++ src/daemon.rs
@@ -1,33 +1,41 @@
-use sysinfo::{Cpu, CpuExt, System, SystemExt};
+use std::sync::{Mutex, MutexGuard};
+use lazy_static::lazy_static;
+use sysinfo::{CpuExt, System, SystemExt};
 
-struct SystemInfo<'a> {
-    cpu: &'a Cpu,
+struct SystemInfo {
+    cpu: String,
     distro: String,
 }
 
+lazy_static! {
+    static ref SYS: Mutex<System> = Mutex::new(System::new_all());
+}
+
 pub(crate) fn main() {
-    let mut sys: System = System::new_all();
+    let mut sys: MutexGuard<System> = SYS.lock().unwrap();
     sys.refresh_all();
 
-    let cpu: &Cpu = get_cpu(&sys);
-    let distro: String = get_distro(&sys);
+    let cpu: String = get_cpu_name();
+    let distro: String = get_distro();
 
     let system_info: SystemInfo = SystemInfo {
         cpu,
         distro,
     };
 
-    let distro = "Distro: ".to_owned() + &*system_info.distro;
-    let cpu = "CPU: ".to_owned() + system_info.cpu.brand();
-    let final_fetch = distro + "\n" + &*cpu;
+    let distro: String = "Distro: ".to_owned() + &*system_info.distro;
+    let cpu: String = "CPU: ".to_owned() + &*system_info.cpu;
+    let final_fetch: String = distro + "\n" + &*cpu;
 
     println!("{}", final_fetch);
 }
 
-fn get_cpu(sys: &System) -> &Cpu {
-    sys.global_cpu_info()
+fn get_cpu_name() -> String {
+    let sys: MutexGuard<System> = SYS.lock().unwrap();
+    sys.global_cpu_info().brand().to_string()
 }
 
-fn get_distro(sys: &System) -> String {
+fn get_distro() -> String {
+    let sys: MutexGuard<System> = SYS.lock().unwrap();
     sys.name().unwrap_or(String::from("Unknown"))
 }
