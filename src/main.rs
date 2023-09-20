29ace66e86915e1be9e0fa10cfb1a3c09c1bbfdf
Notarin <424c414e4b@gmail.com>
2023-09-20 05:11:11 -0400

    Added benchmarking

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,8 @@ use lazy_static::lazy_static;
 struct Args {
     #[arg(long, short, help = "Run as daemon")]
     daemon: bool,
+    #[arg(long, short, help = "On exit print the execution time, for benchmarking")]
+    benchmark: bool,
 }
 
 #[cfg(target_os = "linux")]
@@ -25,6 +27,7 @@ lazy_static!(
 
 #[tokio::main]
 async fn main() {
+    let start: std::time::Instant = std::time::Instant::now();
     let args: Args = Args::parse();
     //daemon mode is the system service that tracks system info
     if args.daemon {
@@ -32,5 +35,8 @@ async fn main() {
         daemon::main().await;
     } else {
         fetch::main();
+    }
+    if args.benchmark {
+        println!("Execution time: {:?}", start.elapsed());
     }
 }
