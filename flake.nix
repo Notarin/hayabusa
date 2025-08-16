127191d92e22fdbda927a8b0340122b291953ab8
Notarin Steele <424c414e4b@gmail.com>
2025-08-16 14:50:08 -0400

    Fixed missing architectures

--- flake.nix
+++ flake.nix
@@ -2,65 +2,76 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     crane.url = "github:ipetkov/crane";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = {
+    self,
     nixpkgs,
     crane,
-    flake-utils,
     ...
-  }:
-    flake-utils.lib.eachDefaultSystemPassThrough (
-      system: let
-        pkgs = nixpkgs.legacyPackages.${system};
-        craneLib = crane.mkLib pkgs;
-      in rec {
-        packages.${system} = rec {
-          hayabusa = craneLib.buildPackage {
-            src = ./.;
-            nativeBuildInputs = with pkgs; [
+  }: (
+    builtins.foldl' (acc: elem: nixpkgs.lib.recursiveUpdate acc elem) {} (
+      builtins.map (
+        system: let
+          pkgs = nixpkgs.legacyPackages.${system};
+          craneLib = crane.mkLib pkgs;
+        in {
+          packages.${system} = rec {
+            hayabusa = craneLib.buildPackage {
+              src = ./.;
+              nativeBuildInputs = with pkgs; [
+                pkg-config
+                openssl
+              ];
+              meta = {
+                homepage = "https://github.com/Notarin/hayabusa/";
+                description = "Hayabusa is a swift rust fetch program.";
+                mainProgram = "hayabusa";
+              };
+            };
+            default = hayabusa;
+          };
+          devShells.${system}.default = craneLib.devShell {
+            packages = with pkgs; [
               pkg-config
               openssl
             ];
-            meta = {
-              homepage = "https://github.com/Notarin/hayabusa/";
-              description = "Hayabusa is a swift rust fetch program.";
-              mainProgram = "hayabusa";
-            };
           };
-          default = hayabusa;
-        };
-        devShells.${system}.default = craneLib.devShell {
-          packages = with pkgs; [
-            pkg-config
-            openssl
-          ];
-        };
-        nixosModules.default = {
-          pkgs,
-          lib,
-          config,
-          ...
-        }: {
-          options = {
-            services.hayabusa = {
-              enable = lib.mkEnableOption "Enable the hayabusa system info daemon";
-            };
+        }
+      )
+      [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "i686-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ]
+    )
+    // {
+      nixosModules.default = {
+        pkgs,
+        lib,
+        config,
+        ...
+      }: {
+        options = {
+          services.hayabusa = {
+            enable = lib.mkEnableOption "Enable the hayabusa system info daemon";
           };
-          config = lib.mkIf config.services.hayabusa.enable {
-            systemd.services.hayabusa = {
-              after = ["network.target"];
-              wants = ["network-online.target"];
-              wantedBy = ["multi-user.target"];
-              serviceConfig = {
-                Restart = "always";
-                Type = "simple";
-                ExecStart = "${lib.getExe packages.${config._module.args.pkgs.stdenv.system}.default} -d";
-              };
+        };
+        config = lib.mkIf config.services.hayabusa.enable {
+          systemd.services.hayabusa = {
+            after = ["network.target"];
+            wants = ["network-online.target"];
+            wantedBy = ["multi-user.target"];
+            serviceConfig = {
+              Restart = "always";
+              Type = "simple";
+              ExecStart = "${lib.getExe self.packages.${config._module.args.pkgs.stdenv.system}.default} -d";
             };
           };
         };
-      }
-    );
+      };
+    }
+  );
 }
